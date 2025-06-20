c976d514b83eb5d4fb365cec516ef211200dc185
Orest Knxwn <orest@200apps.com>
2025-06-21 02:00:57 +0300

    Фикс линта

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,5 +3,5 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  '*.{js,ts,jsx,tsx}': ['eslint --fix --max-warnings=0', 'prettier --write'],
+  '*.{js,ts,jsx,tsx}': ['eslint --fix', 'prettier --write'],
 };
