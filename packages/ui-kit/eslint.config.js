981677d5b025c4dd8fafca58cad101d213cb07a9
Orest Knxwn <orest@200apps.com>
2025-06-21 02:47:51 +0300

    Фикс риннара

--- packages/ui-kit/eslint.config.js
+++ packages/ui-kit/eslint.config.js
@@ -1,11 +1,8 @@
 import { createConfig } from '../../configs/eslint.common.mjs';
 
 export default createConfig({
-  ignores: ['lib/sprite.h.ts'],
-  files: ['.storybook/**/*'],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.storybook.json',
+    project: ['./tsconfig.json', './tsconfig.app.json'],
   },
+  ignores: ['./lib/sprite.h.ts'],
 });
