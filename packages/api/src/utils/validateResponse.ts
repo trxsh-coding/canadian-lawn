6e0d423109cfc445ed62486ded37617b53f72767
Orest Knxwn <orest@200apps.com>
2025-06-20 11:11:42 +0300

    Добавил пофиксил билд и линт

--- packages/api/src/utils/validateResponse.ts
+++ packages/api/src/utils/validateResponse.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export function validateResponse<S extends z.ZodTypeAny>(schema: S, data: unknown): z.infer<S> {
   const result = schema.safeParse(data);
   if (!result.success) {
-    console.log(result);
+    console.error('Validation error:', result.error.format());
     throw new Error('Validation failed');
   }
   return result.data;
