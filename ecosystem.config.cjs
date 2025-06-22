7a9f3191622d10e551a68cdbe2bd28e88a802a9e
Orest Knxwn <orest@200apps.com>
2025-06-22 23:16:11 +0300

    Фикс раннера

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,8 +6,6 @@ module.exports = {
       args: 'workspace @canadian-lawn/backend start',
       cwd: '/var/www/canadian-lawn',
       watch: false,
-      error_file: '/var/www/canadian-lawn/logs/canadian-api-error.log',
-      out_file: '/var/www/canadian-lawn/logs/canadian-api-out.log',
       env: {
         NODE_ENV: 'production',
       },
@@ -18,8 +16,6 @@ module.exports = {
       args: 'workspace @canadian-lawn/web start',
       cwd: '/var/www/canadian-lawn',
       watch: false,
-      error_file: '/var/www/canadian-lawn/logs/canadian-web-error.log',
-      out_file: '/var/www/canadian-lawn/logs/canadian-web-out.log',
       env: {
         NODE_ENV: 'production',
       },
