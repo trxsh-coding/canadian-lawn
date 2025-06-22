61a4ff56b5fba30b0fb231f13280315eed5c221f
Orest Knxwn <orest@200apps.com>
2025-06-22 21:12:06 +0300

    Фикс раннера

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,25 @@
 module.exports = {
   apps: [
     {
-      name: 'canadian-web',
-      script: 'yarn',
-      args: 'serve:web',
+      name: 'canadian-api',
+      script: '/var/www/canadian-lawn/.yarn/releases/yarn-4.9.2.cjs',
+      args: 'workspace @canadian-lawn/backend start',
       cwd: '/var/www/canadian-lawn',
       watch: false,
+      error_file: '/var/www/canadian-lawn/logs/canadian-api-error.log',
+      out_file: '/var/www/canadian-lawn/logs/canadian-api-out.log',
       env: {
         NODE_ENV: 'production',
       },
     },
     {
-      name: 'canadian-api',
-      script: 'yarn',
-      args: 'serve:backend',
+      name: 'canadian-web',
+      script: '/var/www/canadian-lawn/.yarn/releases/yarn-4.9.2.cjs',
+      args: 'workspace @canadian-lawn/web start',
       cwd: '/var/www/canadian-lawn',
       watch: false,
+      error_file: '/var/www/canadian-lawn/logs/canadian-web-error.log',
+      out_file: '/var/www/canadian-lawn/logs/canadian-web-out.log',
       env: {
         NODE_ENV: 'production',
       },
