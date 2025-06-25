da6ff3240e96eca03d17e67528d136fb15349766
Orest Knxwn <orest@200apps.com>
2025-06-25 21:59:31 +0300

    Добавил модель с газонами

--- apps/backend/src/admin/app.tsx
+++ apps/backend/src/admin/app.tsx
@@ -4,6 +4,7 @@ import type { StrapiApp } from '@strapi/strapi/admin';
 export default {
   config: {
     locales: [
+      'ru',
       // 'ar',
       // 'fr',
       // 'cs',
---
+++ apps/backend/src/api/lawn/controllers/lawn.ts
@@ -0,0 +1,7 @@
+/**
+ * lawn controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::lawn.lawn');
